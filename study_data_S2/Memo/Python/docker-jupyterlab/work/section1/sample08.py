1fb403c56d90be628bc4279e2af96c3b482e0c3f
fujishu <shu113104go@docomo.ne.jp>
2025-06-12 01:55:04 +0900

    python学習データ追加

---
+++ study_data_S2/Memo/Python/docker-jupyterlab/work/section1/sample08.py
@@ -0,0 +1,14 @@
+# カプセル化
+class Person:
+  __age = 19 #カプセル化をする変数は__を事前に着ける
+
+  def __init__(self,name,age):
+    self.name = name
+    self.__age = age
+
+  def getAge(self):
+    return self.__age
+
+human = Person("藤原",19)
+
+print(human.getAge())
