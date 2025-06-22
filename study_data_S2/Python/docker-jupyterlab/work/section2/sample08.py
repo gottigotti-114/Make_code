c666cfc562beab449c0061b182f5429e25ee4a11
fujishu <shu113104go@docomo.ne.jp>
2025-06-23 01:28:44 +0900

    railsファイルとDocker関連ファイルの追加

---
+++ study_data_S2/Python/docker-jupyterlab/work/section2/sample08.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+# 仮のデータを用意
+df = pd.DataFrame({
+    "date": pd.date_range(start="2025-01-01", periods=100, freq="D"),
+    "sales": range(100)
+})
+
+# 月ごとの売上合計
+monthly_sales = df.resample("D", on="date")["sales"].sum()
+print(monthly_sales)
